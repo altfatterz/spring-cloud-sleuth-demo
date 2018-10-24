b339bdf29fec5d66cdb48d83d75e5b254cea7348
Zoltan Altfatter <altfatterz@gmail.com>
2018-10-24 12:40:24 +0200

    foo-service also works

--- bar-service/src/main/java/com/example/barservice/BarServiceApplication.java
+++ bar-service/src/main/java/com/example/barservice/BarServiceApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
 @SpringBootApplication
 @RestController
 @Slf4j
@@ -16,9 +19,19 @@ public class BarServiceApplication {
     }
 
     @GetMapping("/")
-    public String bar() {
+    public String bar(HttpServletRequest request) {
+        logHeaders(request);
+
         log.info("bar-service called...");
         return "bar";
+    }
+
+    private void logHeaders(HttpServletRequest request) {
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            log.info(String.format("%s:%s", headerName, request.getHeader(headerName)));
+        }
     }
 
 }
--- foo-service/src/main/java/com/example/fooservice/FooServiceApplication.java
+++ foo-service/src/main/java/com/example/fooservice/FooServiceApplication.java
@@ -1,6 +1,9 @@
 package com.example.fooservice;
 
+import brave.propagation.CurrentTraceContext;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -8,6 +11,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @SpringBootApplication
 public class FooServiceApplication {
@@ -21,11 +27,17 @@ public class FooServiceApplication {
         return builder.build();
     }
 
+    @Bean
+    public CurrentTraceContext currentTraceContext() {
+        return CustomSlf4jCurrentTraceContext.create();
+    }
 }
 
 @RestController
 @Slf4j
 class FooController {
+
+    private final Logger performance = LoggerFactory.getLogger("performance-logger");
 
     private final RestTemplate restTemplate;
 
@@ -34,9 +46,22 @@ class FooController {
     }
 
     @GetMapping("/")
-    public String foo() {
+    public String foo(HttpServletRequest request) {
+
+        logHeaders(request);
+
         log.info("foo-service called...");
+        performance.info("foo-service called...");
+
         String barResponse = restTemplate.getForObject("http://localhost:8082", String.class);
         return "bar response:" + barResponse;
+    }
+
+    private void logHeaders(HttpServletRequest request) {
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while(headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            log.info(String.format("%s:%s", headerName, request.getHeader(headerName)));
+        }
     }
 }
